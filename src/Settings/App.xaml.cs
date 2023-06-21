7dafe4fbff4742a3cc60ac94947581ca21a245b5
Chris Cheetham <chris@cheetham.com>
2023-06-21 14:53:54 -0400

    stub out settings menu

--- src/Settings/App.xaml.cs
+++ src/Settings/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace SettingsPanel
+namespace WpfScratch.Settings
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- src/Settings/MainWindow.xaml.cs
+++ src/Settings/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace SettingsPanel
+namespace WpfScratch.Settings
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
